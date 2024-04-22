cf9a0adb04184496dfb4379706b3e30e4b4a6fa4
James Eastham <dev@jameseastham.co.uk>
2024-04-22 17:14:06 +0100

    Mod 1 updates

--- commit.sh
+++ commit.sh
@@ -1,10 +1,2 @@
-dotnet clean
-
-dotnet restore src/application/PlantBasedPizza.Api/PlantBasedPizza.Api.csproj
-
-dotnet build src/application/PlantBasedPizza.Api/PlantBasedPizza.Api.csproj
-
-dotnet test tests/PlantBasedPizza.UnitTest/PlantBasedPizza.UnitTest.csproj
-
-docker build -f src/PlantBasedPizza.Api/application/PlantBasedPizza.Api/Dockerfile -t plant-based-pizza-api ./src
+docker build -f src/PlantBasedPizza.Api/application/PlantBasedPizza.Api/Dockerfile -t plant-based-pizza-api ./src |
 docker build -f src/PlantBasedPizza.LoyaltyPoints/application/Dockerfile -t loyalty-api ./src
