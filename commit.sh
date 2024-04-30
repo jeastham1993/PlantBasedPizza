4a115a62fcdc9c2c44b0a7f012008d376a10b5e8
James Eastham <dev@jameseastham.co.uk>
2024-04-30 16:32:35 +0100

    Fix compose

--- commit.sh
+++ commit.sh
@@ -1,26 +1,18 @@
-dotnet clean
+docker build -f src/PlantBasedPizza.Orders/application/PlantBasedPizza.Orders.Worker/Dockerfile -t plant-based-orders-worker-mod-6 ./src |
+docker build -f src/PlantBasedPizza.Orders/application/PlantBasedPizza.Orders.Api/Dockerfile -t plant-based-orders-api-mod-6 ./src |
 
-dotnet restore src/application/PlantBasedPizza.Api/PlantBasedPizza.Api.csproj
+docker build -f src/PlantBasedPizza.Kitchen/application/PlantBasedPizza.Kitchen.Worker/Dockerfile -t plant-based-kitchen-worker-mod-6 ./src |
+docker build -f src/PlantBasedPizza.Kitchen/application/PlantBasedPizza.Kitchen.Api/Dockerfile -t plant-based-kitchen-api-mod-6 ./src |
 
-dotnet build src/application/PlantBasedPizza.Api/PlantBasedPizza.Api.csproj
+docker build -f src/PlantBasedPizza.LoyaltyPoints/application/PlantBasedPizza.LoyaltyPoints.Api/Dockerfile -t loyalty-api-mod-6 ./src |
+docker build -f src/PlantBasedPizza.LoyaltyPoints/application/PlantBasedPizza.LoyaltyPoints.Internal/Dockerfile -t loyalty-internal-api-mod-6 ./src |
+docker build -f src/PlantBasedPizza.LoyaltyPoints/application/PlantBasedPizza.LoyaltyPoints.Worker/Dockerfile -t loyalty-worker-mod-6 ./src |
 
-dotnet test tests/PlantBasedPizza.UnitTest/PlantBasedPizza.UnitTest.csproj
+docker build -f src/PlantBasedPizza.Payments/application/PlantBasedPizza.Payments/Dockerfile -t payment-api-mod-6 ./src |
 
-docker build -f src/PlantBasedPizza.Orders/application/PlantBasedPizza.Orders.Worker/Dockerfile -t plant-based-orders-worker ./src |
-docker build -f src/PlantBasedPizza.Orders/application/PlantBasedPizza.Orders.Api/Dockerfile -t plant-based-orders-api ./src |
+docker build -f src/PlantBasedPizza.Recipes/applications/PlantBasedPizza.Recipes.Api/Dockerfile -t plant-based-recipes-api-mod-6 ./src |
 
-docker build -f src/PlantBasedPizza.Kitchen/application/PlantBasedPizza.Kitchen.Worker/Dockerfile -t plant-based-kitchen-worker ./src |
-docker build -f src/PlantBasedPizza.Kitchen/application/PlantBasedPizza.Kitchen.Api/Dockerfile -t plant-based-kitchen-api ./src |
+docker build -f src/PlantBasedPizza.Delivery/application/PlantBasedPizza.Delivery.Worker/Dockerfile -t plant-based-delivery-worker-mod-6 ./src |
+docker build -f src/PlantBasedPizza.Delivery/application/PlantBasedPizza.Delivery.Api/Dockerfile -t plant-based-delivery-api-mod-6 ./src
 
-docker build -f src/PlantBasedPizza.LoyaltyPoints/application/PlantBasedPizza.LoyaltyPoints.Api/Dockerfile -t loyalty-api ./src |
-docker build -f src/PlantBasedPizza.LoyaltyPoints/application/PlantBasedPizza.LoyaltyPoints.Internal/Dockerfile -t loyalty-internal-api ./src |
-docker build -f src/PlantBasedPizza.LoyaltyPoints/application/PlantBasedPizza.LoyaltyPoints.Worker/Dockerfile -t loyalty-worker ./src |
-
-docker build -f src/PlantBasedPizza.Payments/application/PlantBasedPizza.Payments/Dockerfile -t payment-api ./src |
-
-docker build -f src/PlantBasedPizza.Recipes/applications/PlantBasedPizza.Recipes.Api/Dockerfile -t plant-based-recipes-api ./src |
-
-docker build -f src/PlantBasedPizza.Delivery/application/PlantBasedPizza.Delivery.Worker/Dockerfile -t plant-based-delivery-worker ./src |
-docker build -f src/PlantBasedPizza.Delivery/application/PlantBasedPizza.Delivery.Api/Dockerfile -t plant-based-delivery-api ./src
-
-docker build -f src/PlantBasedPizza.Account/application/PlantBasedPizza.Account.Api/Dockerfile -t plant-based-account-api ./src
+docker build -f src/PlantBasedPizza.Account/application/PlantBasedPizza.Account.Api/Dockerfile -t plant-based-account-api-mod-6 ./src
