62dc7a793d476201cf3ed126d35660305b69fc72
James Eastham <dev@jameseastham.co.uk>
2024-05-10 09:27:52 +0100

    Add deployment of shared services

--- shared-infrastructure/lib/shared-infrastructure-stack.ts
+++ shared-infrastructure/lib/shared-infrastructure-stack.ts
@@ -3,6 +3,7 @@ import { Construct } from 'constructs';
 import { Network } from './network';
 import { EventBus } from 'aws-cdk-lib/aws-events';
 import { ApplicationListener, ApplicationLoadBalancer, ListenerAction} from 'aws-cdk-lib/aws-elasticloadbalancingv2';
+import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 
 export class PlantBasedPizzaSharedInfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -25,5 +26,30 @@ export class PlantBasedPizzaSharedInfrastructureStack extends cdk.Stack {
     const eventBus = new EventBus(this, "PlantBasedPizzaEventBus", {
       eventBusName: 'PlantBasedPizzaEvents'
     })
+    
+    const albEndpointParameter = new StringParameter(this, "ALBEndpointParam", {
+      stringValue: `http://${sharedAlbWithListener.loadBalancerDnsName}`,
+      parameterName: '/shared/alb-endpoint'
+    });
+
+    const albArnParameter = new StringParameter(this, "ALBArnParam", {
+      stringValue: sharedAlbWithListener.loadBalancerArn,
+      parameterName: '/shared/alb-arn'
+    });
+
+    const listenerArnParameter = new StringParameter(this, "ListenerArnParam", {
+      stringValue: httpListener.listenerArn,
+      parameterName: '/shared/alb-listener'
+    });
+
+    const eventBusName = new StringParameter(this, "EventBusNameParam", {
+      stringValue: eventBus.eventBusName,
+      parameterName: '/shared/eb-name'
+    });
+
+    const ebArnParam = new StringParameter(this, "EventBusArnParam", {
+      stringValue: eventBus.eventBusArn,
+      parameterName: '/shared/eb-arn'
+    });
   }
 }
