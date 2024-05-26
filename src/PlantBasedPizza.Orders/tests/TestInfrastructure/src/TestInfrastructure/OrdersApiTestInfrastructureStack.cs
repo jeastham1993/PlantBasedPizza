5bedd6933e2982712521a41d246fccca54a44526
James Eastham <dev@jameseastham.co.uk>
2024-05-26 17:08:57 +0100

    Add DLQ's to tests

--- src/PlantBasedPizza.Orders/tests/TestInfrastructure/src/TestInfrastructure/OrdersApiTestInfrastructureStack.cs
+++ src/PlantBasedPizza.Orders/tests/TestInfrastructure/src/TestInfrastructure/OrdersApiTestInfrastructureStack.cs
@@ -28,59 +28,23 @@ public class OrdersApiTestInfrastructureStack : Stack
         });
 
         var ordersTestSource = "https://orders.test.plantbasedpizza/";
-        
-        var preparingQueue = MapEventToTestQueue(bus, "OrderPreparingQueue", stackProps, ordersTestSource, "kitchen.orderPreparing.v1");
-        var prepCompleteQueue = MapEventToTestQueue(bus, "OrderPrepCompleteQueue", stackProps, ordersTestSource, "kitchen.orderPrepComplete.v1");
-        var bakedQueue = MapEventToTestQueue(bus, "OrderBakedQueue", stackProps, ordersTestSource, "kitchen.orderBaked.v1");
-        var orderQualityCheckedQueue = MapEventToTestQueue(bus, "OrderQualityCheckedQueue", stackProps, "https://tests.orders/", "kitchen.orderQualityChecked.v1");
-        var driverDeliveredQueue = MapEventToTestQueue(bus, "DriverDeliveredOrderQueue", stackProps, ordersTestSource, "delivery.driverDeliveredOrder.v1");
-        var driverCollectedQueue = MapEventToTestQueue(bus, "DriverCollectedOrderQueue", stackProps, ordersTestSource, "delivery.driverCollectedOrder.v1");
+
+        var preparingQueue = new EventQueue(this, "OrderPreparingQueue", new EventQueueProps(bus, "OrderPreparingQueue", "dev", ordersTestSource, "kitchen.orderPreparing.v1"));
+        var prepCompleteQueue = new EventQueue(this, "OrderPrepCompleteQueue", new EventQueueProps(bus, "OrderPrepCompleteQueue", "dev", ordersTestSource, "kitchen.orderPrepComplete.v1"));
+        var bakedQueue = new EventQueue(this, "OrderBakedQueue", new EventQueueProps(bus, "OrderBakedQueue", "dev", ordersTestSource, "kitchen.orderBaked.v1"));
+        var orderQualityCheckedQueue = new EventQueue(this, "OrderQualityCheckedQueue", new EventQueueProps(bus, "OrderQualityCheckedQueue", "dev", ordersTestSource, "kitchen.orderQualityChecked.v1"));
+        var driverDeliveredQueue = new EventQueue(this, "DriverDeliveredOrderQueue", new EventQueueProps(bus, "DriverDeliveredOrderQueue", "dev", ordersTestSource, "delivery.driverDeliveredOrder.v1"));
+        var driverCollectedQueue = new EventQueue(this, "DriverCollectedOrderQueue", new EventQueueProps(bus, "DriverCollectedOrderQueue", "dev", ordersTestSource, "delivery.driverCollectedOrder.v1"));
         var loyaltyPointsQueue = new EventQueue(this, "LoyaltyUpdatedQueue", new EventQueueProps(bus, "LoyaltyUpdatedQueue", "dev", ordersTestSource, "loyalty.customerLoyaltyPointsUpdated.v1"));
 
         var backgroundWorker = new BackgroundWorker(this, "OrdersWorkerTestFunction",
             new BackgroundWorkerProps(new SharedInfrastructureProps(null, bus, null, "int-test", "dev"),
-                "../../application", databaseConnectionParam, loyaltyPointsQueue.Queue, driverCollectedQueue, driverDeliveredQueue, bakedQueue, prepCompleteQueue, preparingQueue, orderQualityCheckedQueue));
+                "../../application", databaseConnectionParam, loyaltyPointsQueue.Queue, driverCollectedQueue.Queue, driverDeliveredQueue.Queue, bakedQueue.Queue, prepCompleteQueue.Queue, preparingQueue.Queue, orderQualityCheckedQueue.Queue));
 
         var eventBus = new CfnOutput(this, "EBOutput", new CfnOutputProps()
         {
             ExportName = $"EventBusName-{stackProps.Version}",
             Value = bus.EventBusName
         });
-        var loyaltyUpdatedQueueOutput = new CfnOutput(this, "LoyaltyQueueUrl", new CfnOutputProps()
-        {
-            ExportName = $"LoyaltyPointsUpdatedQueueUrl-{stackProps.Version}",
-            Value = loyaltyPointsQueue.Queue.QueueUrl
-        });
-        var qualityCheckedQueueOutput = new CfnOutput(this, "QueueUrlOutput", new CfnOutputProps()
-        {
-            ExportName = $"OrderQualityCheckedQueueUrl-{stackProps.Version}",
-            Value = orderQualityCheckedQueue.QueueUrl
-        });
-    }
-
-    private Queue MapEventToTestQueue(EventBus bus, string queueName, ApplicationStackProps stackProps, string eventSource, string detailType)
-    {
-        if (!eventSource.EndsWith('/'))
-        {
-            eventSource += "/";
-        }
-        
-        var queue = new Queue(this, $"{queueName}-{stackProps.Version}", new QueueProps()
-        {
-            QueueName = $"{queueName}-{stackProps.Version}"
-        });
-
-        var rule = new Rule(this, $"{queueName}Rule", new RuleProps()
-        {
-            EventBus = bus
-        });
-        rule.AddEventPattern(new EventPattern()
-        {
-            Source = [eventSource],
-            DetailType = [detailType]
-        });
-        rule.AddTarget(new SqsQueue(queue));
-        
-        return queue;
     }
 }
