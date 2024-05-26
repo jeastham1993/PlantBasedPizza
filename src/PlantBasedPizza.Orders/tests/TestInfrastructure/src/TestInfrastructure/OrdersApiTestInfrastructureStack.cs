1dc6d3949560062c0d0dc0a2bacc7640a158ec24
James Eastham <dev@jameseastham.co.uk>
2024-05-26 17:18:31 +0100

    Update SQS queue to use commit hash

--- src/PlantBasedPizza.Orders/tests/TestInfrastructure/src/TestInfrastructure/OrdersApiTestInfrastructureStack.cs
+++ src/PlantBasedPizza.Orders/tests/TestInfrastructure/src/TestInfrastructure/OrdersApiTestInfrastructureStack.cs
@@ -29,13 +29,13 @@ public class OrdersApiTestInfrastructureStack : Stack
 
         var ordersTestSource = "https://orders.test.plantbasedpizza/";
 
-        var preparingQueue = new EventQueue(this, "OrderPreparingQueue", new EventQueueProps(bus, "OrderPreparingQueue", "dev", ordersTestSource, "kitchen.orderPreparing.v1"));
-        var prepCompleteQueue = new EventQueue(this, "OrderPrepCompleteQueue", new EventQueueProps(bus, "OrderPrepCompleteQueue", "dev", ordersTestSource, "kitchen.orderPrepComplete.v1"));
-        var bakedQueue = new EventQueue(this, "OrderBakedQueue", new EventQueueProps(bus, "OrderBakedQueue", "dev", ordersTestSource, "kitchen.orderBaked.v1"));
-        var orderQualityCheckedQueue = new EventQueue(this, "OrderQualityCheckedQueue", new EventQueueProps(bus, "OrderQualityCheckedQueue", "dev", ordersTestSource, "kitchen.orderQualityChecked.v1"));
-        var driverDeliveredQueue = new EventQueue(this, "DriverDeliveredOrderQueue", new EventQueueProps(bus, "DriverDeliveredOrderQueue", "dev", ordersTestSource, "delivery.driverDeliveredOrder.v1"));
-        var driverCollectedQueue = new EventQueue(this, "DriverCollectedOrderQueue", new EventQueueProps(bus, "DriverCollectedOrderQueue", "dev", ordersTestSource, "delivery.driverCollectedOrder.v1"));
-        var loyaltyPointsQueue = new EventQueue(this, "LoyaltyUpdatedQueue", new EventQueueProps(bus, "LoyaltyUpdatedQueue", "dev", ordersTestSource, "loyalty.customerLoyaltyPointsUpdated.v1"));
+        var preparingQueue = new EventQueue(this, "OrderPreparingQueue", new EventQueueProps(bus, "OrderPreparingQueue", stackProps.Version, ordersTestSource, "kitchen.orderPreparing.v1"));
+        var prepCompleteQueue = new EventQueue(this, "OrderPrepCompleteQueue", new EventQueueProps(bus, "OrderPrepCompleteQueue", stackProps.Version, ordersTestSource, "kitchen.orderPrepComplete.v1"));
+        var bakedQueue = new EventQueue(this, "OrderBakedQueue", new EventQueueProps(bus, "OrderBakedQueue", stackProps.Version, ordersTestSource, "kitchen.orderBaked.v1"));
+        var orderQualityCheckedQueue = new EventQueue(this, "OrderQualityCheckedQueue", new EventQueueProps(bus, "OrderQualityCheckedQueue", stackProps.Version, ordersTestSource, "kitchen.orderQualityChecked.v1"));
+        var driverDeliveredQueue = new EventQueue(this, "DriverDeliveredOrderQueue", new EventQueueProps(bus, "DriverDeliveredOrderQueue", stackProps.Version, ordersTestSource, "delivery.driverDeliveredOrder.v1"));
+        var driverCollectedQueue = new EventQueue(this, "DriverCollectedOrderQueue", new EventQueueProps(bus, "DriverCollectedOrderQueue", stackProps.Version, ordersTestSource, "delivery.driverCollectedOrder.v1"));
+        var loyaltyPointsQueue = new EventQueue(this, "LoyaltyUpdatedQueue", new EventQueueProps(bus, "LoyaltyUpdatedQueue", stackProps.Version, ordersTestSource, "loyalty.customerLoyaltyPointsUpdated.v1"));
 
         var backgroundWorker = new BackgroundWorker(this, "OrdersWorkerTestFunction",
             new BackgroundWorkerProps(new SharedInfrastructureProps(null, bus, null, "int-test", stackProps.Version),
