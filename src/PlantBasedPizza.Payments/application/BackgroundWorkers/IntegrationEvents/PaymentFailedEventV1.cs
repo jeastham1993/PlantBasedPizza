5603f92dfc06a343850313100b2b3bc47409d6ad
James Eastham <dev@jameseastham.co.uk>
2024-05-31 10:10:34 +0100

    Update payment service event source

--- src/PlantBasedPizza.Payments/application/BackgroundWorkers/IntegrationEvents/PaymentFailedEventV1.cs
+++ src/PlantBasedPizza.Payments/application/BackgroundWorkers/IntegrationEvents/PaymentFailedEventV1.cs
@@ -7,7 +7,7 @@ public class PaymentFailedEventV1 : IntegrationEvent
     public override string EventName => "payments.paymentFailed";
     public override string EventVersion => "v1";
     
-    public override Uri Source => new("https://payments.plantbasedpizza.com");
+    public override Uri Source => new("https://payments.plantbasedpizza");
     
     public string CustomerIdentifier { get; init; }
     public string OrderIdentifier { get; init; }
