9dd0ff467ad2b06f8f0b2127ece131a5b0e9dc19
james.eastham <james.eastham@datadoghq.com>
2024-06-25 22:01:50 -0400

    Update Java event publisher to use CloudEvents

--- src/PlantBasedPizza.Recipes.Java/src/core/src/main/java/com/recipe/messaging/EventBridgeEventPublisher.java
+++ src/PlantBasedPizza.Recipes.Java/src/core/src/main/java/com/recipe/messaging/EventBridgeEventPublisher.java
@@ -6,14 +6,24 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.recipe.core.RecipeCreatedEventV1;
 import com.recipe.core.RecipeDeletedEventV1;
 import com.recipe.core.RecipeUpdatedEventV1;
+import io.cloudevents.CloudEvent;
+import io.cloudevents.core.builder.CloudEventBuilder;
+import io.cloudevents.core.format.ContentType;
+import io.cloudevents.core.provider.EventFormatProvider;
+import io.cloudevents.jackson.JsonCloudEventData;
+import io.opentracing.SpanContext;
+import io.opentracing.util.GlobalTracer;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsRequest;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsRequestEntry;
 import software.amazon.awssdk.services.eventbridge.model.PutEventsResponse;
 
+import java.net.URI;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class EventBridgeEventPublisher {
@@ -30,21 +40,7 @@ public class EventBridgeEventPublisher {
 
     public void publish(RecipeCreatedEventV1 evt) {
         try {
-            PutEventsRequestEntry reqEntry = PutEventsRequestEntry.builder()
-                    .source(this.SOURCE)
-                    .detailType("recipe.recipeCreated.v1")
-                    .detail(_mapper.writeValueAsString(evt))
-                    .eventBusName(System.getenv("EVENT_BUS_NAME"))
-                    .build();
-
-            List<PutEventsRequestEntry> eventList = new ArrayList<>();
-            eventList.add(reqEntry);
-
-            PutEventsRequest eventsRequest = PutEventsRequest.builder()
-                    .entries(reqEntry)
-                    .build();
-
-            PutEventsResponse result = _eventBridge.putEvents(eventsRequest);
+            this.publishEvent("recipe.recipeCreated.v1", _mapper.writeValueAsBytes(evt));
         }
         catch (JsonProcessingException e){
         }
@@ -52,21 +48,7 @@ public class EventBridgeEventPublisher {
 
     public void publish(RecipeUpdatedEventV1 evt) {
         try {
-            PutEventsRequestEntry reqEntry = PutEventsRequestEntry.builder()
-                    .source(this.SOURCE)
-                    .detailType("recipe.recipeUpdated.v1")
-                    .detail(_mapper.writeValueAsString(evt))
-                    .eventBusName(System.getenv("EVENT_BUS_NAME"))
-                    .build();
-
-            List<PutEventsRequestEntry> eventList = new ArrayList<>();
-            eventList.add(reqEntry);
-
-            PutEventsRequest eventsRequest = PutEventsRequest.builder()
-                    .entries(reqEntry)
-                    .build();
-
-            PutEventsResponse result = _eventBridge.putEvents(eventsRequest);
+            this.publishEvent("recipe.recipeUpdated.v1", _mapper.writeValueAsBytes(evt));
         }
         catch (JsonProcessingException e){
         }
@@ -74,23 +56,45 @@ public class EventBridgeEventPublisher {
 
     public void publish(RecipeDeletedEventV1 evt) {
         try {
-            PutEventsRequestEntry reqEntry = PutEventsRequestEntry.builder()
-                    .source(this.SOURCE)
-                    .detailType("recipe.recipeDeleted.v1")
-                    .detail(_mapper.writeValueAsString(evt))
-                    .eventBusName(System.getenv("EVENT_BUS_NAME"))
-                    .build();
-
-            List<PutEventsRequestEntry> eventList = new ArrayList<>();
-            eventList.add(reqEntry);
-
-            PutEventsRequest eventsRequest = PutEventsRequest.builder()
-                    .entries(reqEntry)
-                    .build();
-
-            PutEventsResponse result = _eventBridge.putEvents(eventsRequest);
+            this.publishEvent("recipe.recipeDeleted.v1", _mapper.writeValueAsBytes(evt));
         }
         catch (JsonProcessingException e){
         }
+    }
+    
+    private void publishEvent(String type, byte[] contents) {
+        SpanContext currentSpan = GlobalTracer.get().activeSpan().context();
+
+        CloudEvent evtWrapper = CloudEventBuilder
+                .v03()
+                .withId(UUID.randomUUID().toString())
+                .withType("recipe.recipeCreated.v1")
+                .withSource(URI.create(this.SOURCE))
+                .withData("application/json", contents)
+                .withTime(OffsetDateTime.now())
+                .withExtension("ddtraceid", currentSpan.toTraceId())
+                .withExtension("ddspanid", currentSpan.toSpanId())
+                .build();
+
+        byte[] serialized = EventFormatProvider
+                .getInstance()
+                .resolveFormat(ContentType.JSON)
+                .serialize(evtWrapper);
+
+        PutEventsRequestEntry reqEntry = PutEventsRequestEntry.builder()
+                .source(this.SOURCE)
+                .detailType("recipe.recipeCreated.v1")
+                .detail(new String(serialized))
+                .eventBusName(System.getenv("EVENT_BUS_NAME"))
+                .build();
+
+        List<PutEventsRequestEntry> eventList = new ArrayList<>();
+        eventList.add(reqEntry);
+
+        PutEventsRequest eventsRequest = PutEventsRequest.builder()
+                .entries(reqEntry)
+                .build();
+
+        PutEventsResponse result = _eventBridge.putEvents(eventsRequest);
     }
 }
