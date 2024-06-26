b6ce1e53a279bcaaf5b8ccc724b03950012921e4
james.eastham <james.eastham@datadoghq.com>
2024-06-25 22:15:57 -0400

    Update health check endpoint

--- src/PlantBasedPizza.Recipes.Java/src/api/src/main/java/com/recipe/api/HealthController.java
+++ src/PlantBasedPizza.Recipes.Java/src/api/src/main/java/com/recipe/api/HealthController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
-    @GetMapping("/recipe/health")
+    @GetMapping("/recipes/health")
     ResponseEntity<String> healthCheck() {
         return ResponseEntity.ok("");
     }
