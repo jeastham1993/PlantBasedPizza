1e97040f36190801008655ba2cdb07faf7cd24de
James Eastham <dev@jameseastham.co.uk>
2024-06-30 17:00:01 +0100

    Update Java handler

--- src/PlantBasedPizza.Recipes.Java/src/api/src/main/java/com/recipe/api/HealthController.java
+++ src/PlantBasedPizza.Recipes.Java/src/api/src/main/java/com/recipe/api/HealthController.java
@@ -1,7 +1,5 @@
 package com.recipe.api;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
