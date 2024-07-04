20c779a316aae8baaf60d71ce9e08a71330564e1
james.eastham <james.eastham@datadoghq.com>
2024-07-04 14:20:38 +0100

    Update function signature

--- src/PlantBasedPizza.Recipes.Java/src/functions/src/main/java/com/recipe/functions/FunctionConfiguration.java
+++ src/PlantBasedPizza.Recipes.Java/src/functions/src/main/java/com/recipe/functions/FunctionConfiguration.java
@@ -5,16 +5,14 @@ import com.amazonaws.services.lambda.runtime.events.SQSBatchResponse;
 import com.recipe.core.IRecipeRepository;
 import datadog.trace.api.Trace;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.function.context.FunctionRegistration;
-import org.springframework.cloud.function.context.catalog.FunctionTypeUtils;
-import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.GenericApplicationContext;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 @SpringBootApplication(scanBasePackages = "com.recipe")
 public class FunctionConfiguration{
@@ -26,24 +24,26 @@ public class FunctionConfiguration{
     }
     
     @Bean
-    public SQSBatchResponse handleOrderConfirmedEvent(SQSEvent evt) {
-        List<SQSBatchResponse.BatchItemFailure> failures = new ArrayList<>();
+    public Function<SQSEvent, SQSBatchResponse> handleOrderConfirmedEvent(SQSEvent evt) {
+        return value -> {
+            List<SQSBatchResponse.BatchItemFailure> failures = new ArrayList<>();
 
-        List<SQSEvent.SQSMessage> records = evt.getRecords();
+            List<SQSEvent.SQSMessage> records = evt.getRecords();
 
-        for (int i = 0, recordsSize = records.size(); i < recordsSize; i++) {
-            SQSEvent.SQSMessage message = records.get(i);
+            for (int i = 0, recordsSize = records.size(); i < recordsSize; i++) {
+                SQSEvent.SQSMessage message = records.get(i);
 
-            var processSuccess = processOrderConfirmedMessage(message);
+                var processSuccess = processOrderConfirmedMessage(message);
 
-            if (!processSuccess) {
-                failures.add(SQSBatchResponse.BatchItemFailure.builder().withItemIdentifier(message.getMessageId()).build());
+                if (!processSuccess) {
+                    failures.add(SQSBatchResponse.BatchItemFailure.builder().withItemIdentifier(message.getMessageId()).build());
+                }
             }
-        }
-
-        return SQSBatchResponse.builder()
-                .withBatchItemFailures(failures)
-                .build();
+            
+            return SQSBatchResponse.builder()
+                    .withBatchItemFailures(failures)
+                    .build();
+        };
     }
 
     @Trace(operationName = "ProcessingOrderConfirmedMessage", resourceName = "FunctionConfiguration.processOrderConfirmedMessage")
