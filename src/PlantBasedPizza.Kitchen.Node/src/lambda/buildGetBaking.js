e8758d66bc806080ddbcb847a736afc01db8b555
james.eastham <james.eastham@datadoghq.com>
2024-07-08 16:44:40 +0100

    Add esbuild files for Node Lambda

---
+++ src/PlantBasedPizza.Kitchen.Node/src/lambda/buildGetBaking.js
@@ -0,0 +1,33 @@
+const ddPlugin = require('dd-trace/esbuild')
+const esbuild = require('esbuild')
+
+esbuild.build({
+  entryPoints: ['./src/lambda/getBaking.ts'],
+  bundle: true,
+  minify: true,
+  outfile: 'out/getBaking/index.js',
+  plugins: [ddPlugin],
+  platform: 'node', // allows built-in modules to be required
+  target: ['node20'],
+  external: [
+    // esbuild cannot bundle native modules
+    '@datadog/native-metrics',
+
+    // required if you use profiling
+    '@datadog/pprof',
+
+    // required if you use Datadog security features
+    '@datadog/native-appsec',
+    '@datadog/native-iast-taint-tracking',
+    '@datadog/native-iast-rewriter',
+
+    // required if you encounter graphql errors during the build step
+    'graphql/language/visitor',
+    'graphql/language/printer',
+    'graphql/utilities',
+    '@aws-sdk/client-sqs'
+  ]
+}).catch((err) => {
+  console.error(err)
+  process.exit(1)
+})
