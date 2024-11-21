3a1236749508fa4d0fdb7da36e41f243282a844c
james.eastham <james.eastham@datadoghq.com>
2024-11-21 10:26:46 +0000

    Add logs to auth middleware

--- src/PlantBasedPizza.Payments/application/PlantBasedPizza.Payments/ApiKeyAuthenticationMiddleware.cs
+++ src/PlantBasedPizza.Payments/application/PlantBasedPizza.Payments/ApiKeyAuthenticationMiddleware.cs
@@ -4,18 +4,21 @@ public class ApiKeyAuthenticationMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly APIKeyProvider _apiKeyProvider;
+    private readonly ILogger<ApiKeyAuthenticationMiddleware> _logger;
     private const string APIKEYNAME = "APIKey";
     
-    public ApiKeyAuthenticationMiddleware(RequestDelegate next, APIKeyProvider apiKeyProvider)
+    public ApiKeyAuthenticationMiddleware(RequestDelegate next, APIKeyProvider apiKeyProvider, ILogger<ApiKeyAuthenticationMiddleware> logger)
     {
         _next = next;
         _apiKeyProvider = apiKeyProvider;
+        _logger = logger;
     }
 
     public async Task InvokeAsync(HttpContext context)
     {
         if (!context.Request.Headers.TryGetValue(APIKEYNAME, out var extractedApiKey))
         {
+            this._logger.LogInformation("API Key not provided");
             context.Response.StatusCode = 401;
             await context.Response.WriteAsync("API Key was not provided. ");
             return;
@@ -25,8 +28,10 @@ public class ApiKeyAuthenticationMiddleware
 
         if (!_apiKeyProvider.IsValidApiKey(providedApiKey))
         {
+            this._logger.LogInformation("Unauthorized");
+            
             context.Response.StatusCode = 403;
-            await context.Response.WriteAsync("Unauthorized client. ");
+            await context.Response.WriteAsync("Unauthorized client.");
             return;
         }
 
