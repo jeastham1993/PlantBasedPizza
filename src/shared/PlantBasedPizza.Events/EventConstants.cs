08a5982a2b9657940d2f082f8a1ea925bea238b9
James Eastham <dev@jameseastham.co.uk>
2024-11-29 19:55:34 +0000

    Add constants for event metadata keys

---
+++ src/shared/PlantBasedPizza.Events/EventConstants.cs
@@ -0,0 +1,8 @@
+namespace PlantBasedPizza.Events;
+
+public class EventConstants
+{
+    public const string EVENT_SOURCE_HEADER_KEY = "cloudevent.source";
+    public const string EVENT_TYPE_HEADER_KEY = "cloudevent.type";
+    public const string EVENT_ID_HEADER_KEY = "cloudevent.id";
+}
