b8db4da4e0ea57830bacac42953987d02f8672c7
james.eastham <james.eastham@datadoghq.com>
2024-12-12 16:33:01 +0000

    Add environment specific infra and payments test harness

--- infra/environment/data.tf
+++
@@ -1,2 +0,0 @@
-data "azurerm_subscription" "primary" {
-}
---
+++ infra/module/environment/variables.tf
@@ -0,0 +1,15 @@
+variable "subscription_id" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
+
+variable "dd_site" {
+    type = string
+}
+
+variable "dd_api_key" {
+    type = string
+}
