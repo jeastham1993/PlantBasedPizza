0bf4fac6d7d28f8d8510889dac91d909ee2db272
James Eastham <dev@jameseastham.co.uk>
2024-12-13 08:01:59 +0000

    Update dockerfile and test driver code

--- infra/payments/main.tf
+++ infra/payments/main.tf
@@ -138,3 +138,141 @@ resource "azurerm_container_app" "payment-api" {
     }
   }
 }
+
+resource "azurerm_container_app" "payment-test-harness" {
+  name                         = "payment-test-harness"
+  container_app_environment_id = data.azurerm_container_app_environment.env.id
+  resource_group_name          = data.azurerm_resource_group.plant_based_pizza_rg.name
+  revision_mode                = "Single"
+  count = var.env == "payments-test" ? 1 : 0
+  dapr {
+    app_id = "payment-test-harness"
+    app_port = 8080
+    app_protocol = "http"
+  }
+  identity {
+    identity_ids = [ azurerm_user_assigned_identity.app_identity.id ]
+    type = "UserAssigned"
+  }
+  ingress {
+    external_enabled = true
+    target_port = 8080
+    traffic_weight {
+      percentage = 100
+      latest_revision = true
+    }
+  }
+  template {
+    min_replicas = 1
+    max_replicas = 1
+    container {
+      name   = "application"
+      image  = "plantpowerjames/plant-based-pizza-payment-event-harness:${var.app_version}"
+      cpu    = 0.25
+      memory = "0.5Gi"
+      env {
+        name = "Environment"
+        value = var.env
+      }
+      env {
+        name = "DOMAIN"
+        value = "payments"
+      }
+      env {
+        name = "ApplicationConfig__TeamName"
+        value = "payments"
+      }
+      env {
+        name = "ApplicationConfig__ApplicationName"
+        value = "payments-api"
+      }
+      env {
+        name = "ApplicationConfig__Environment"
+        value = var.env
+      }
+      env {
+        name = "ApplicationConfig__Version"
+        value = var.app_version
+      }
+      env {
+        name = "ApplicationConfig__DeployedAt"
+        value = var.app_version
+      }
+      env {
+        name = "ApplicationConfig__MemoryMb"
+        value = "500"
+      }
+      env {
+        name = "ApplicationConfig__CpuCount"
+        value = "0.25"
+      }
+      env {
+        name = "ApplicationConfig__CloudRegion"
+        value = "europe-west2"
+      }
+      env {
+        name = "MOMENTO_API_KEY"
+        value = var.momento_api_key
+      }
+      env {
+        name = "CACHE_NAME"
+        value = var.cache_name
+      }
+      env {
+        name  = "OTEL_EXPORTER_OTLP_ENDPOINT"
+        value = "http://localhost:4317"
+      }
+    }
+    container {
+      name   = "datadog"
+      image  = "index.docker.io/datadog/serverless-init:latest"
+      cpu    = 0.25
+      memory = "0.5Gi"
+
+      env {
+        name = "DD_SITE"
+        value = var.dd_site
+      }
+      env {
+        name = "DD_API_KEY"
+        value = var.dd_api_key
+      }
+      env {
+        name = "DD_ENV"
+        value = var.env
+      }
+      env {
+        name = "DD_VERSION"
+        value = var.app_version
+      }
+      env {
+        name = "DD_SERVICE"
+        value = "payments"
+      }
+      env {
+        name = "DD_LOGS_ENABLED"
+        value = "true"
+      }
+      env {
+        name = "DD_LOGS_INJECTION"
+        value = "true"
+      }
+      env {
+        name = "DD_APM_IGNORE_RESOURCES"
+        value = "/opentelemetry.proto.collector.trace.v1.TraceService/Export$"
+      }
+      env {
+        name = "DD_OTLP_CONFIG_RECEIVER_PROTOCOLS_GRPC_ENDPOINT"
+        value = "0.0.0.0:4317"
+      }
+      env {
+        name = "DD_AZURE_SUBSCRIPTION_ID"
+        value = data.azurerm_subscription.primary.subscription_id
+      }
+      env {
+        name = "DD_AZURE_RESOURCE_GROUP"
+        value = data.azurerm_resource_group.plant_based_pizza_rg.name
+      }
+    }
+  }
+}
--- src/PlantBasedPizza.Payments/tests/PlantBasedPizza.Payments.DeveloperTests/IntegrationTestDriver.cs
+++ src/PlantBasedPizza.Payments/tests/PlantBasedPizza.Payments.DeveloperTests/IntegrationTestDriver.cs
@@ -11,15 +11,17 @@ public class IntegrationTestDriver : ITestDriver
     public IntegrationTestDriver()
     {
         _client = new HttpClient();
-        _client.BaseAddress = new Uri("http://localhost:8081");
+        _client.BaseAddress =
+            new Uri(Environment.GetEnvironmentVariable("TEST_HARNESS_ENDPOINT") ?? "http://localhost:8081");
     }
-    
-    public async Task<HttpResponseMessage> TakePaymentWithValidBody(string orderIdentifier, decimal amount, string? eventId = null)
+
+    public async Task<HttpResponseMessage> TakePaymentWithValidBody(string orderIdentifier, decimal amount,
+        string? eventId = null)
     {
         if (eventId is null)
             eventId = Guid.NewGuid().ToString();
-        
-        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, $"/events/take-payment");
+
+        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, "/events/take-payment");
         httpRequestMessage.Content = JsonContent.Create(new
         {
             OrderIdentifier = orderIdentifier,
@@ -31,13 +33,14 @@ public class IntegrationTestDriver : ITestDriver
 
         return await _client.SendAsync(httpRequestMessage);
     }
-    
-    public async Task<HttpResponseMessage> TakePaymentWithInvalidPaymentAmount(string orderIdentifier, string? eventId = null)
+
+    public async Task<HttpResponseMessage> TakePaymentWithInvalidPaymentAmount(string orderIdentifier,
+        string? eventId = null)
     {
         if (eventId is null)
             eventId = Guid.NewGuid().ToString();
-        
-        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, $"/events/take-payment");
+
+        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, "/events/take-payment");
         httpRequestMessage.Content = JsonContent.Create(new
         {
             OrderIdentifier = orderIdentifier,
@@ -49,13 +52,14 @@ public class IntegrationTestDriver : ITestDriver
 
         return await _client.SendAsync(httpRequestMessage);
     }
-    
-    public async Task<HttpResponseMessage> TakePaymentWithInvalidBody(string orderIdentifier, decimal amount, string? eventId = null)
+
+    public async Task<HttpResponseMessage> TakePaymentWithInvalidBody(string orderIdentifier, decimal amount,
+        string? eventId = null)
     {
         if (eventId is null)
             eventId = Guid.NewGuid().ToString();
-        
-        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, $"/events/take-payment");
+
+        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, "/events/take-payment");
         httpRequestMessage.Headers.Add(EventConstants.EVENT_ID_HEADER_KEY, eventId);
         httpRequestMessage.Content = JsonContent.Create(new
         {
@@ -70,8 +74,8 @@ public class IntegrationTestDriver : ITestDriver
 
     public async Task<int> VerifySuccessEventReceivedFor(VerificationOptions options)
     {
-        var events = await this.GetEventsFor(options.OrderIdentifier);
-        
+        var events = await GetEventsFor(options.OrderIdentifier);
+
         var successEvent = events.Count(evt => evt.EventName == "PaymentSuccessfulEventV1");
 
         return successEvent;
@@ -79,27 +83,20 @@ public class IntegrationTestDriver : ITestDriver
 
     public async Task<int> VerifyFailureEventReceivedFor(VerificationOptions options)
     {
-        var events = await this.GetEventsFor(options.OrderIdentifier);
-        
+        var events = await GetEventsFor(options.OrderIdentifier);
+
         var successEvent = events.Count(evt => evt.EventName == "PaymentFailedEventV1");
 
         return successEvent;
     }
 
-    public async Task<int> VerifyNoEventIsReceived(string orderIdentifier)
-    {
-        var events = await this.GetEventsFor(orderIdentifier);
-
-        return events.Count;
-    }
-    
     private async Task<List<ReceivedEvent>> GetEventsFor(string orderIdentifier)
     {
         await Task.Delay(TimeSpan.FromSeconds(2));
         var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, $"/events/{orderIdentifier}");
 
         var responseMessage = await _client.SendAsync(httpRequestMessage);
-        
+
         return JsonSerializer.Deserialize<List<ReceivedEvent>>(await responseMessage.Content.ReadAsStringAsync())!;
     }
 }
