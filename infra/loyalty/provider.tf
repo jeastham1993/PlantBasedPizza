6488accfde92a6c24030f190e487027e5c4e8081
james.eastham <james.eastham@datadoghq.com>
2025-01-21 14:06:30 +0000

    Update provider for Azure services

--- infra/loyalty/provider.tf
+++ infra/loyalty/provider.tf
@@ -9,7 +9,7 @@ terraform {
   }
   backend "azurerm" {
       resource_group_name  = "terraform-state"
-      storage_account_name = "jeasthamterraformstate"
+      storage_account_name = "plantbasedpizzatfstate"
       container_name       = "plantbasedpizza"
       key                  = "loyalty.tfstate"
   }
