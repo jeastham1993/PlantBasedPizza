1a9ab323bafbfa92881516f0ce863442df365ece
james.eastham <james.eastham@datadoghq.com>
2025-02-28 07:48:46 +0000

    Update payment services

--- src/PlantBasedPizza.Payments/tests/PlantBasedPizza.Payments.Management/Program.cs
+++ src/PlantBasedPizza.Payments/tests/PlantBasedPizza.Payments.Management/Program.cs
@@ -7,7 +7,16 @@ using Azure.ResourceManager.AppContainers.Models;
 var armClient = new ArmClient(new DefaultAzureCredential());
 var subscription = await armClient.GetDefaultSubscriptionAsync();
 var resourceGroups = subscription.GetResourceGroups();
-var resourceGroup = await resourceGroups.GetAsync("plant-based-pizza-payments-test");
+
+Console.WriteLine("Enter the resource group name (defaults to 'plant-based-pizza-payments-test':");
+var resourceGroupName = Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(resourceGroupName))
+{
+    resourceGroupName = "plant-based-pizza-payments-test";
+}
+
+var resourceGroup = await resourceGroups.GetAsync(resourceGroupName);
 var paymentContainerApp = resourceGroup.Value.GetContainerApps();
 
 var runMode = Environment.GetEnvironmentVariable("RUN_MODE") ?? "stop";
@@ -24,17 +33,17 @@ await foreach (var app in paymentContainerApp.GetAllAsync())
         
         await app.StartAsync(WaitUntil.Completed);
         
-        var runningState = RevisionRunningState.Unknown.ToString();
+        var runningState = (latestRevision.Value.Data.RunningState ?? RevisionRunningState.Unknown).ToString();
 
         while (runningState != RevisionRunningState.Running && runningState != "RunningAtMaxScale")
         {
-            latestRevision = await app.GetContainerAppRevisionAsync(app.Data.LatestRevisionName);
-
+            await app.GetContainerAppRevisionAsync(app.Data.LatestRevisionName);
+            
             runningState = (latestRevision.Value.Data.RunningState ?? RevisionRunningState.Unknown).ToString();
 
             Console.WriteLine($"Running state is: {runningState}");
 
-            await Task.Delay(TimeSpan.FromSeconds(10));
+            await Task.Delay(TimeSpan.FromSeconds(1));
         }
     }
     else
