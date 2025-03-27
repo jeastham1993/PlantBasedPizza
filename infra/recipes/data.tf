6ef0a8f5311a87f5caea38fa187e2c73e3e59f17
james.eastham <james.eastham@datadoghq.com>
2025-03-27 13:41:33 +0000

    Update spacing

--- infra/recipes/data.tf
+++ infra/recipes/data.tf
@@ -9,7 +9,6 @@ data "azurerm_container_app_environment" "env" {
   name                = var.env
   resource_group_name = var.resource_group_name
 }
-
 data "azurerm_servicebus_namespace" "public_service_bus" {
   name                = var.public_service_bus_namespace
   resource_group_name = var.resource_group_name
